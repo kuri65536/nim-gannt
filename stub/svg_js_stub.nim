7b496628e5ab26d4fa1c4c5b2e0d555ae8e4e6aa
Shimoda <kuri65536@hotmail.com>
2018-12-11 08:39:07 +0900

    append license text at the top of files.

--- stub/svg_js_stub.nim
+++ stub/svg_js_stub.nim
@@ -1,3 +1,9 @@
+# Copyright (c) 2018, Shimoda <kuri65536 at hotmail dot com>
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
 type
   SvgJs* = ref SvgJsObj
   SvgJsObj {.importc.} = object of RootObj
